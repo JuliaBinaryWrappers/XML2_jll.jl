1cf5f76460f1421ba75b50758874a5bd5e79f5d8
jlbuild <juliabuildbot@gmail.com>
2026-06-02 09:07:24 +0000

    XML2_jll build 2.15.3+0

--- src/XML2_jll.jl
+++ src/XML2_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("XML2")
-JLLWrappers.@generate_main_file("XML2", UUID("02c8fc9c-b97f-50b9-bbe4-9be30ff0a78a"))
+JLLWrappers.@generate_main_file("XML2", Base.UUID("02c8fc9c-b97f-50b9-bbe4-9be30ff0a78a"))
 end  # module XML2_jll
